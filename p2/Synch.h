2f04a0fa2b2659c6542bbd83d3c4e28a5096bc3e
Sahel Mesforoush <5176161-sahel.m4@users.noreply.gitlab.com>
2021-03-28 17:51:17 +0430

    initial commit

---
+++ p2/Synch.h
@@ -0,0 +1,40 @@
+header Synch
+
+  uses Thread
+
+  class Semaphore
+    superclass Object
+    fields
+      count: int
+      waitingThreads: List [Thread]
+    methods
+      Init (initialCount: int)Sahel Mesforoush
+      Down ()
+      Up ()
+  endClass
+
+  class Mutex
+    superclass Object
+    fields
+      isLocked: bool -- boolean specifying current condition
+      ownerThread: ptr to Thread -- used for checking if current thread is holding the lock
+      waitingThreads: List [Thread] -- list of threads waiting for the lock
+    methods
+      Init ()
+      Lock ()
+      Unlock ()
+      IsHeldByCurrentThread () returns bool
+  endClass
+
+  class Condition
+    superclass Object
+    fields
+      waitingThreads: List [Thread]
+    methods
+      Init ()
+      Wait (mutex: ptr to Mutex)
+      Signal (mutex: ptr to Mutex)
+      Broadcast (mutex: ptr to Mutex)
+  endClass
+
+endHeader
